6c850df59d261c5fd953936d92fe1407d1f7a69c
Fabio Luis <ml@fabiotecnico1.biz>
2022-02-02 18:53:37 -0300

    React Project - ChatPage - Backend - Stickers implemented

--- pages/chat.js
+++ pages/chat.js
@@ -3,6 +3,7 @@ import React from 'react';
 import appConfig from '../config.json';
 import { useRouter } from 'next/router';
 import { createClient } from '@supabase/supabase-js'
+import { ButtonSendSticker } from '../src/components/ButtonSendSticker';
 
 const SUPABASE_ANON_KEY = ''
 const SUPABASE_URL = ''
@@ -135,6 +136,7 @@ export default function ChatPage() {
                                 color: appConfig.theme.colors.neutrals[200],
                             }}
                         />
+                        <ButtonSendSticker />
                     </Box>
                 </Box>
             </Box>
